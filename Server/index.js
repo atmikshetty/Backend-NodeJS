dd8435c835b748deebd360fa5eeff4345ff4eee0
atmikshetty <atmikshetty10@student.sfit.ac.in>
2024-04-30 22:25:30 +0530

    Express with NodeJS

--- Server/index.js
+++ Server/index.js
@@ -1,50 +1,20 @@
-// Creating a mini logger of reqs recieved
+const express = require('express');
 
-const http = require('http')
-const fs = require('fs');
-const url = require('url');
+const app = express();
 
-const atmikServer = http.createServer((req,res) => {
-
-    // Ignoring the initial request made by node
-    if(req.url === "/favicon.ico") return res.end();
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
-
-    const log = `${Date.now()}: ${req.url} ${req.method} New Request Recieved \n`
-    fs.appendFile('log.txt', log, (err, data)=> {
- 
-        switch(myUrl.pathname){
-            case '/':
-                if(req.method === 'GET') res.end("Home Page")
-                break;
-            case '/about':
-                const name = myUrl.query.myName;
-                const rollNo = myUrl.query.rollNo;
-                res.end(`Hello ${name} Roll No: ${rollNo}`)
-                break;
-            case '/contacts':
-                res.end("Welcome to contact page")
-                break;
-            case '/signup':
-                if(req.method === 'GET') res.end("Welcome to contact page")
-                else if(req.method === 'POST'){
-                    // DB CONNECTION CODE
-                    res.end('Sucessfully sent data!!!')
-            }
-                break;
-            default:
-                res.end('404 Route not found')
-        }
-
-        // if(err){
-        //     console.log(err);
-        // }else{
-        //     res.end('Hello From The Server');
-        // }
-    });
-});
-
-atmikServer.listen(8000, () => {
-    console.log('Server Active');
+app.get('/', (req,res) => {
+    res.send("Welcome "+ req.query.name+ "To Home page");
 })
+
+app.get('/about', (req,res) => {
+    res.send("Welcome "+ req.query.name+ "To Home page");
+})
+
+app.listen(8000, ()=> console.log('Success'));
+
+// Not Required with Express
+// const atmikServer = http.createServer(app);
+
+// atmikServer.listen(8000, () => {
+//     console.log('Server Active');
+// })
